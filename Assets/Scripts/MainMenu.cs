7f2fa8bf44eda316133c1fc30df1c0eced0bbbc6
teworcapt <tesa10nieto@gmail.com>
2023-11-17 16:56:23 +0800

    1.0 Scenes and Animation Update
    Finished all scenes designs, player movement, enemy & player animation.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void StartGame()
+    {
+
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# L0V3L0rN
+ L0V3L0rN hopes to be a turn-based RPG world following the protagonist on trying to overcome the world infected by 'L0V3'.
